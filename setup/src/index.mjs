d60fb5dfcadb8fbf3b085d5b1aaab125778d4d6b
Philippe Assis <codephilippe@gmail.com>
2023-08-12 18:29:20 -0300

    save

--- setup/src/index.mjs
+++ setup/src/index.mjs
@@ -6,17 +6,13 @@ import {
 import core from '@actions/core';
 
 const { stage, stageUpperCase } = getStageVariables();
-
-core.setOutput('stage', stage);
-core.setOutput('stage-name', stageUpperCase);
-
 const projectName = process.env.PROJECT_NAME
 const stackName = getStackName(stage, projectName)
-
-core.setOutput('stack-name', stackName);
-
 const bucketName = `${cameCaseToDash(projectName)}-lambda-artifacts`;
 
 console.log(`Setting repo name to ${bucketName}`);
 
+core.setOutput('stage', stage);
+core.setOutput('stage-name', stageUpperCase);
+core.setOutput('stack-name', stackName);
 core.setOutput('lambda-artifacts', bucketName);
